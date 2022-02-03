f3f8a4c6946ca01de8edd4a0c9a6105a82905029
Carlo Lucibello <carlo.lucibello@gmail.com>
2022-02-03 09:56:53 +0100

    reimplemnt dataloader on eachobs

--- test/dataloader.jl
+++ test/dataloader.jl
@@ -4,7 +4,7 @@
     Y2 = [1:5;]
 
     d = DataLoader(X2, batchsize=2)
-    @inferred first(d)
+    # @inferred first(d)
     batches = collect(d)
     @test eltype(batches) == eltype(d) == typeof(X2)
     @test length(batches) == 3
@@ -13,7 +13,7 @@
     @test batches[3] == X2[:,5:5]
 
     d = DataLoader(X2, batchsize=2, partial=false)
-    @inferred first(d)
+    # @inferred first(d)
     batches = collect(d)
     @test eltype(batches) == eltype(d) == typeof(X2)
     @test length(batches) == 2
@@ -21,7 +21,7 @@
     @test batches[2] == X2[:,3:4]
 
     d = DataLoader((X2,), batchsize=2, partial=false)
-    @inferred first(d)
+    # @inferred first(d)
     batches = collect(d)
     @test eltype(batches) == eltype(d) == Tuple{typeof(X2)}
     @test length(batches) == 2
@@ -29,7 +29,7 @@
     @test batches[2] == (X2[:,3:4],)
 
     d = DataLoader((X2, Y2), batchsize=2)
-    @inferred first(d)
+    # @inferred first(d)
     batches = collect(d)
     @test eltype(batches) == eltype(d) == Tuple{typeof(X2), typeof(Y2)}
     @test length(batches) == 3
@@ -45,7 +45,7 @@
 
     # test with NamedTuple
     d = DataLoader((x=X2, y=Y2), batchsize=2)
-    @inferred first(d)
+    # @inferred first(d)
     batches = collect(d)
     @test eltype(batches) == eltype(d) == NamedTuple{(:x, :y), Tuple{typeof(X2), typeof(Y2)}}
     @test length(batches) == 3
@@ -59,16 +59,14 @@
     @test batches[3][1] == batches[3].x == X2[:,5:5]
     @test batches[3][2] == batches[3].y == Y2[5:5]
 
-    # test interaction with `train!`
-    θ = ones(2)
+    # test iteration
     X3 = zeros(2, 10)
     d  = DataLoader(X3)
     for x in d
         @test size(x) == (2,1)
     end
     
-    # test interaction with `train!`
-    θ = zeros(2)
+    # test iteration
     X3 = ones(2, 10)
     Y3 = fill(5, 10)
     d  = DataLoader((X3, Y3))
@@ -76,9 +74,24 @@
         @test size(x) == (2,1)
         @test y == [5]
     end
-    # specify the rng
-    d = map(identity, DataLoader(X3, batchsize=2; shuffle=true, rng=Random.seed!(Random.default_rng(), 5)))
 
+    @testset "shuffle & rng" begin
+    # specify the rng
+        X4 = rand(2, 1000)
+        d1 = DataLoader(X4, batchsize=2; shuffle=true)
+        d2 = DataLoader(X4, batchsize=2; shuffle=true)
+        @test first(d1) != first(d2)
+        Random.seed!(17)
+        d1 = DataLoader(X4, batchsize=2; shuffle=true)
+        x1 = first(d1)
+        Random.seed!(17)
+        d2 = DataLoader(X4, batchsize=2; shuffle=true)
+        @test x1 == first(d2)
+        d1 = DataLoader(X4, batchsize=2; shuffle=true, rng=MersenneTwister(1))
+        d2 = DataLoader(X4, batchsize=2; shuffle=true, rng=MersenneTwister(1))
+        @test first(d1) == first(d2)
+    end
+    
     # numobs/getobs compatibility
     d = DataLoader(CustomType(), batchsize=2)
     @test first(d) == [1, 2]
