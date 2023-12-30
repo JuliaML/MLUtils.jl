76f7e1389b39104c701ba3965b6a114fdc9303c5
Carlo Lucibello <carlo.lucibello@gmail.com>
2023-12-30 18:05:13 +0100

    Update src/parallel.jl
    Co-authored-by: Brian Chen <ToucheSir@users.noreply.github.com>

--- src/parallel.jl
+++ src/parallel.jl
@@ -223,7 +223,7 @@ function Base.put!(f!, ringbuffer::RingBuffer)
     return buf_
 end
 
-Base.put!(::Channel{T}, ::MLUtils.RingBuffer) = error("method not defined for RingBuffer")
+Base.put!(c::Channel{T}, b::MLUtils.RingBuffer) = throw(MethodError(put!, (c, b)))
 
 function Base.close(ringbuffer::RingBuffer, args...)
     close(ringbuffer.results, args...)
