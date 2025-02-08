964b5d8154c174ee9cfd312658368dc2e892ee41
Carlo Lucibello <carlo.lucibello@gmail.com>
2025-02-08 09:54:20 +0100

    add guides

--- src/obsview.jl
+++ src/obsview.jl
@@ -197,29 +197,37 @@ Base.parent(x::ObsView) = x.data
 """
     obsview(data, [indices])
 
-Returns a lazy view of the observations in `data` that
-correspond to the given `indices`. No data will be copied except
-of the indices. It is similar to constructing an [`ObsView`](@ref), 
-but returns a `SubArray` if the type of
-`data` is `Array` or `SubArray`. Furthermore, this function may
-be extended for custom types of `data` that also want to provide
-their own subset-type.
+Return a lazy view of the observations in `data` that
+correspond to the given `indices`. No data will be copied. 
 
-In case `data` is a tuple, the constructor will be mapped
-over its elements. That means that the constructor returns a
-tuple of `ObsView` instead of a `ObsView` of tuples.
+By default the return is an [`ObsView`](@ref), although this can be
+overloaded for custom types of `data` that want to provide
+their own lazy view.
 
-If instead you want to get the subset of observations
-corresponding to the given `indices` in their native type, use
-`getobs`.
+In case `data` is a tuple or named tuple, the constructor will be mapped
+over its elements. For array types, return a subarray.
 
-See [`ObsView`](@ref) for more information.
+The observation in the returned view `ov` can be materialized by calling
+`getobs(ov, i)` on the view, where `i` is an index in `1:length(ov)`.
+
+If `indices` is not provided, it will be assumed to be `1:numobs(data)`.
+```
+
+# Examples
+
+```jldoctest
+julia> obsview([1 2 3; 4 5 6], 1:2)
+2×2 view(::Matrix{Int64}, :, 1:2) with eltype Int64:
+ 1  2
+ 4  5
+```
 """
-obsview(data, indices=1:numobs(data)) = ObsView(data, indices)
+obsview(data, indices) = ObsView(data, indices)
+obsview(data) = obsview(data, 1:numobs(data))
 
 ##### Arrays / SubArrays
 
-obsview(A::SubArray) = A
+obsview(A::AbstractArray) = A
 
 function obsview(A::AbstractArray{T,N}, idx) where {T,N}
     I = ntuple(_ -> :, N-1)
